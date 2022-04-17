a8785ba75e7571dfd2c59fd1bf715021ae3d7fa4
Chaos <antonioscutari2@hotmail.com>
2022-04-17 10:45:33 -0300

    [ADD] Landing done

--- client/src/Components/Landing.jsx
+++ client/src/Components/Landing.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Styles/Landing.css";
 
 const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <div className="Front">
+    <div className="GLASS">
+      <h1 className="Font">GAMER'S CAVE</h1>
+      <Link to="/home">
+        <button className="Button">Press START</button>
+      </Link>
+    </div>
+    </div>
+  );
+};
 
-export default Landing
+export default Landing;
