e95d7aba38770d09607fa82be143f7552f22cb2f
Chaos <antonioscutari2@hotmail.com>
2022-04-17 08:46:26 -0300

    [ADD] Index Routing and Redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./store/index.js";
+import Landing from "./Components/Landing";
+import Home from "./Components/Home";
+import Creator from "./Components/Creator";
+import Details from "./Components/Details";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Home/>} />
+          <Route path="/home/:id" element={<Details />}/>
+          <Route path="/create" element={<Creator />} />
+        </Routes>
+    </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
