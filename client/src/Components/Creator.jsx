dbad3f0eb26f3d88be70aa1d877a5ea4f34b899c
Chaos <antonioscutari2@hotmail.com>
2022-04-21 14:05:58 -0300

    [FIX] Creator now rendering

--- client/src/Components/Creator.jsx
+++ client/src/Components/Creator.jsx
@@ -2,6 +2,7 @@ import React, { useEffect,useState } from 'react'
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { createGame, getGenres } from '../store/actions';
+import Loader from './Loader'
 
 function validateForms (input)
 {
@@ -74,118 +75,121 @@ const Creator = () => {
   useEffect(() => {
     dispatch(getGenres());
   }, [dispatch]);
-
-  return (
-    <div>
-    <h1> GAME to be created:</h1>
-  <form onSubmit={(e) => handleSubmit(e)}>
-    <div>
+  if(gens.length){
+    return (
+      <div>
+      <h1> GAME to be created:</h1>
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <div>
+        <label>
+              Name:
+        <input
+        type="text"
+        value={input.name}
+        name="name"
+        required
+        onChange={(e) => handleChange(e)}
+        />
+        {error.name && <p>{error.name} </p>}
+        </label>
+      </div>
+      <div>
       <label>
-            Name:
-      <input
-      type="text"
-      value={input.name}
-      name="name"
-      required
-      onChange={(e) => handleChange(e)}
+              Description:
+        <input
+        type="text"
+        value={input.description}
+        name="description"
+        required
+        onChange={(e) => handleChange(e)}
       />
-      {error.name && <p>{error.name} </p>}
+      {error.description && <p>{error.description} </p>}
       </label>
-    </div>
-    <div>
-    <label>
-            Description:
-      <input
-      type="text"
-      value={input.description}
-      name="description"
-      required
-      onChange={(e) => handleChange(e)}
-    />
-    {error.description && <p>{error.description} </p>}
-    </label>
-    </div>
-    <div>
-      <label>
-            Image:
-        <input
-          type="text"
-          value={input.image}
-          name="image"
-          onChange={(e) => handleChange(e)}
-        />
-      </label>
-    </div>
-    <div>
-      <label>
-            Rating:
-        <input
-          type="number"
-          min="1"
-          max="5"
-          value={input.rating}          
-          name="rating"
-          required
-          onChange={(e) => handleChange(e)}
-        />
-        {error.rating && <p>{error.rating} </p>}
-      </label>
-    </div>
-    <div>
-      <label>
-           Released:
-        <input
-          type="text"
-          required
-          value={input.releaseDate}
-          name="releaseDate"
-          onChange={(e) => handleChange(e)}
-        />
-        {error.releaseDate && <p>{error.releaseDate} </p>}
-      </label>
-    </div>
-    <div>
-      <label>
-            Platforms:
-        <input
-          type="text"
-          value={input.platforms}
-          name="platforms"
-          required
-          onChange={(e) => handleChange(e)}
-        />
-        {error.platforms && <p>{error.platforms} </p>}
-      </label>
-    </div>
-        Genres:
-        <select onChange={(e) => handleSelect(e)}>
-          {gens?.map((el) => (
-            <option value={el.name}>
-              {el.name}
-            </option>
+      </div>
+      <div>
+        <label>
+              Image:
+          <input
+            type="text"
+            value={input.image}
+            name="image"
+            onChange={(e) => handleChange(e)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+              Rating:
+          <input
+            type="number"
+            min="1"
+            max="5"
+            value={input.rating}          
+            name="rating"
+            required
+            onChange={(e) => handleChange(e)}
+          />
+          {error.rating && <p>{error.rating} </p>}
+        </label>
+      </div>
+      <div>
+        <label>
+             Released:
+          <input
+            type="text"
+            required
+            value={input.releaseDate}
+            name="releaseDate"
+            onChange={(e) => handleChange(e)}
+          />
+          {error.releaseDate && <p>{error.releaseDate} </p>}
+        </label>
+      </div>
+      <div>
+        <label>
+              Platforms:
+          <input
+            type="text"
+            value={input.platforms}
+            name="platforms"
+            required
+            onChange={(e) => handleChange(e)}
+          />
+          {error.platforms && <p>{error.platforms} </p>}
+        </label>
+      </div>
+          Genres:
+          <select onChange={(e) => handleSelect(e)}>
+            {gens?.map((el) => (
+              <option value={el.name}>
+                {el.name}
+              </option>
+            ))}
+          </select>
+          {error.gens && <p>{error.gens} </p>}
+          <ul>
+          {input.gens?.map((el) => (
+            <div>
+              <button onClick={() => handleDelete(el)}>x</button>
+              <p>{el}</p>
+            </div>
           ))}
-        </select>
-        {error.gens && <p>{error.gens} </p>}
-        <ul>
-        {input?.gens.map((el) => (
-          <div>
-            <button onClick={() => handleDelete(el)}>x</button>
-            <p>{el}</p>
-          </div>
-        ))}
-        </ul>
-        
-  <button className="butt" type="submit">
-    Create Recipe
-  </button>
-  <Link to="/home">
-    <button className="butt">Back</button>
-  </Link>
-  <input type="text" placeholder="Paste the ID of a recipe" onChange={(e) => handleChange(e)}
-  />
-      </form>
-  </div>
-  )
+          </ul>
+          
+    <button className="butt" type="submit">
+      Create Recipe
+    </button>
+    <Link to="/home">
+      <button className="butt">Back</button>
+    </Link>
+    <input type="text" placeholder="Paste the ID of a recipe" onChange={(e) => handleChange(e)}
+    />
+        </form>
+    </div>
+    )
+  }else{
+    return(<Loader/>)
+  }
 }
 
 export default Creator
