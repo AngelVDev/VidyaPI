60348bd977c988761542a072239a8933543ff222
Chaos <antonioscutari2@hotmail.com>
2022-05-14 20:10:27 -0300

    [ADD] Minor changes and styles

--- client/src/Components/Cards.jsx
+++ client/src/Components/Cards.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import "./Styles/Cards.css";
 
-const Cards = ({image, name, genres}) => {
+const Cards = ({ image, name, genres }) => {
   return (
-      <div id="container">
-        <div id="info">
+    <div id="container">
+      <div id="info">
         <h1>{name}</h1>
-        <p>Genres: {genres?.map((el)=> el.name + ";  ")}</p>
-        </div>
-        <img src={image} alt="cardimgerror" />
+        <p>
+          Genres:{" "}
+          {genres?.map((el) => (
+            <span id="genR">{el.name}</span>
+          ))}
+        </p>
       </div>
+      <img src={image} alt="cardimgerror" />
+    </div>
   );
 };
 export default Cards;
