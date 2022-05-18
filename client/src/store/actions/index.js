872220018accb3340fb5131b5cda623a7ce8267f
Chaos <antonioscutari2@hotmail.com>
2022-05-18 01:25:04 -0300

    [ADD] More actions

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-export function getVideogames(){
-  return function(dispatch){
-           axios.get("http://localhost:3001/videogames")
-          .then(response => {
-              return dispatch({
-                  type: "GET_GAMES",
-                  payload: response.data
-              })
-  })
-}}
+export function getVideogames() {
+  return function (dispatch) {
+    axios.get("http://localhost:3001/videogames").then((response) => {
+      return dispatch({
+        type: "GET_GAMES",
+        payload: response.data,
+      });
+    });
+  };
+}
 // export function getVideogames() {
 //   return async function (dispatch) {
 //     const response = await axios.get("http://localhost:3001/videogames");
@@ -24,16 +24,19 @@ export function getGenres() {
   };
 }
 export function getQVideogames(name) {
-  return async function(dispatch){
+  return async function (dispatch) {
     try {
-        var json = await axios.get(`http://localhost:3001/videogames?name=${name}`)
-        return dispatch({
-            type: "GET_QUERY",
-            payload: json.data
-        })  
+      var json = await axios.get(
+        `http://localhost:3001/videogames?name=${name}`
+      );
+      return dispatch({
+        type: "GET_QUERY",
+        payload: json.data,
+      });
     } catch (error) {
-        console.log(error)
-    }}
+      console.log(error);
+    }
+  };
 }
 export function getDetails(id) {
   return async function (dispatch) {
@@ -45,27 +48,67 @@ export function getDetails(id) {
   };
 }
 export let createGame = (payload) => {
-    console.log("Soy un peiloud", payload);
-    return async (dispatch) => {
-      try {
-        let json = await axios.post("http://localhost:3001/videogames", payload);
-        return dispatch({
-          type: "POST_GAME",
-          payload: json.data,
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  console.log("Soy un peiloud", payload);
+  return async (dispatch) => {
+    try {
+      let json = await axios.post("http://localhost:3001/videogames", payload);
+      return dispatch({
+        type: "POST_GAME",
+        payload: json.data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
+};
 
-export function deleteById(id){
-  return async function(dispatch){
-    try{
-      const json = await axios.get(`http://localhost:3001/${id}/delete`)
-      return dispatch({ type: "DELETE_BY_ID", payload: json.data})
-    }catch(err){
-      console.log(err)
+export function orderByName(payload) {
+  return async (dispatch) => {
+    dispatch({
+      type: "SORT_NAME",
+      payload,
+    });
+  };
+}
+export function showCreated(payload) {
+  return async (dispatch) => {
+    try {
+      dispatch({
+        type: "FILTER_SOURCE",
+        payload,
+      });
+    } catch (err) {
+      console.log(err);
     }
+  };
+}
+export function filterByGenre(payload) {
+  return async (dispatch) => {
+    try {
+      dispatch({
+        type: "FILTER_GENRES",
+        payload,
+      });
+    } catch (err) {
+      return console.log(err);
     }
+  };
+}
+export function orderByRating(payload) {
+  return async (dispatch) =>
+    dispatch({
+      type: "SORT_RATING",
+      payload,
+    });
+}
+
+export function deleteById(id) {
+  return async function (dispatch) {
+    try {
+      const json = await axios.get(`http://localhost:3001/${id}/delete`);
+      return dispatch({ type: "DELETE_BY_ID", payload: json.data });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 }
