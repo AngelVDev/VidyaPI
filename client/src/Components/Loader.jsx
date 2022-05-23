df75152fad37db48c9e1450b6fd74f35250fc187
Chaos <antonioscutari2@hotmail.com>
2022-05-23 04:27:24 -0300

    [ADD] Background div

--- client/src/Components/Loader.jsx
+++ client/src/Components/Loader.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import "./Styles/Loader.css";
 
 const Loader = () => {
   return (
-    <div className="spinner">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div id="LoaderContainer">
+      <div className="spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
