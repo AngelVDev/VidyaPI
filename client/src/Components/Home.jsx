72d0fd8d80fed039a893de876d6f2870f0a9fcf5
Angel Scutari <88778765+AngelVDev@users.noreply.github.com>
2023-10-15 04:22:02 -0300

    fix?: fetchs

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { getVideogames } from "../store/actions";
+import { getVideogames, getGenres } from "../store/actions";
 import "./Styles/Home.css";
 import Cards from "./Cards";
 import Loader from "./Loader";
@@ -13,7 +13,7 @@ import { ButtonT, StyledLink } from "./Styles/Styled";
 
 const Home = () => {
   const games = useSelector((state) => state.allGames);
-  const gens = useSelector((state) => state?.genres);
+  const gens = useSelector((state) => state.genres);
   const dispatch = useDispatch();
   /*<--Pagination-->*/
   const [currentPage, setCurrentPage] = useState(1);
@@ -26,59 +26,66 @@ const Home = () => {
     setCurrentPage(pageNum);
   };
 
-  useEffect(() => {
-    dispatch(getVideogames());
-  }, [dispatch]);
-
-  if (games && gens) {
-    return (
-      <div key={"parent"} className="Wall">
-        <video loop autoPlay>
-          <source src={vid} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <h3 key={"h3"} className="Welcome">
-          The sight of all this games, fills you with determination 🌟
-        </h3>
-        <nav key={"justNav"} className="Navi">
-          <ButtonT key={"butCreate"}>
-            <StyledLink style={{ textDecoration: "none" }} to="/create">
-              Let's create a new one
-            </StyledLink>
-          </ButtonT>
-          <Filters gens={gens} setCurrentPage={setCurrentPage} />
-          <Searchbar />
-        </nav>
-        <div key={"cCont"} id="cardContainer">
-          {games &&
-            currentGames?.map((GG) => {
-              return (
-                <Link
-                  key={"thoseLinks." + GG.id}
-                  style={{ textDecoration: "none" }}
-                  to={"/home/" + GG.id}
-                >
-                  <Cards
-                    name={GG.name}
-                    image={GG.image}
-                    genres={GG.genres}
-                    rating={GG.rating}
-                    key={GG.id}
-                  />
-                </Link>
-              );
-            })}
-        </div>
-        <Pagination
-          gamesPerPage={gamesPerPage}
-          games={games.length}
-          pagination={PAGINATION}
-        />
-      </div>
-    );
-  } else {
-    return <Loader />;
+  const fetchAll = () => {
+    if(!games && !gens){
+      getGenres()
+      getVideogames()
+    }
   }
+
+useEffect(() => {
+  fetchAll()
+}, []);
+
+if (games && gens) {
+  return (
+    <div key={"parent"} className="Wall">
+      <video loop autoPlay>
+        <source src={vid} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <h3 key={"h3"} className="Welcome">
+        The sight of all this games, fills you with determination 🌟
+      </h3>
+      <nav key={"justNav"} className="Navi">
+        <ButtonT key={"butCreate"}>
+          <StyledLink style={{ textDecoration: "none" }} to="/create">
+            Let's create a new one
+          </StyledLink>
+        </ButtonT>
+        <Filters gens={gens} setCurrentPage={setCurrentPage} />
+        <Searchbar />
+      </nav>
+      <div key={"cCont"} id="cardContainer">
+        {games &&
+          currentGames?.map((GG) => {
+            return (
+              <Link
+                key={"thoseLinks." + GG.id}
+                style={{ textDecoration: "none" }}
+                to={"/home/" + GG.id}
+              >
+                <Cards
+                  name={GG.name}
+                  image={GG.image}
+                  genres={GG.genres}
+                  rating={GG.rating}
+                  key={GG.id}
+                />
+              </Link>
+            );
+          })}
+      </div>
+      <Pagination
+        gamesPerPage={gamesPerPage}
+        games={games.length}
+        pagination={PAGINATION}
+      />
+    </div>
+  );
+} else {
+  return <Loader />;
+}
 };
 
 export default Home;
