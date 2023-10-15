e92914c7f41bf6dca0b1e5fa8cc0a39a6bbfe57c
Angel Scutari <88778765+AngelVDev@users.noreply.github.com>
2023-10-15 04:04:05 -0300

    upd: actions baseUrl

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.BACK_URL ?? "http://localhost:3001";
+axios.defaults.baseURL = process.env.BACK_URL;
 
 export function getVideogames() {
   return function (dispatch) {
